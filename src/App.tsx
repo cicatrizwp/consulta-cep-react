fc16eab3e77614e9bbd31f9baf3674e51d6dbe32
Pedro Mello <pedrocmello.bsi@gmail.com>
2019-10-30 22:13:55 -0300

    edit: importando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/Button/Button';
+import InputField from './components/InputField/InputField';
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Button />
+          <InputField />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
