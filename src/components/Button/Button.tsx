b1c53dd8481231a880ef050b5956ad0ae5be8a1f
Pedro Mello <pedrocmello.bsi@gmail.com>
2019-10-30 22:41:45 -0300

    feat: adicionando estilo no button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.button`
+  border-radius: 5px;
+  background: #7e7aec;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: .35rem 1rem;
+  
+  &:hover {
+    border: 1px solid #7e7aec;
+  }
+`
 
 const Button = () =>
-  <div>
-    Hello
-  </div>
+  <Wrapper>
+    Buscar
+  </Wrapper>
 
 export default Button;
